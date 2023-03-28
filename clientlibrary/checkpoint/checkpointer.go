02d4b44ff64b61ba35964ab3aede7ee358f406d9
Shiva Pentakota <spentakota@vmware.com>
2023-03-28 11:46:55 -0700

    fix: add shutdown and leaseExpired error cases for checkpoint function
    Signed-off-by: Shiva Pentakota <spentakota@vmware.com>

--- clientlibrary/checkpoint/checkpointer.go
+++ clientlibrary/checkpoint/checkpointer.go
@@ -79,6 +79,9 @@ type Checkpointer interface {
 	// RemoveLeaseOwner to remove lease owner for the shard entry to make the shard available for reassignment
 	RemoveLeaseOwner(string) error
 
+	// GetLeaseOwner to get current owner of lease for shard
+	GetLeaseOwner(string) (string, error)
+
 	// ListActiveWorkers returns active workers and their shards (New Lease Stealing Methods)
 	ListActiveWorkers(map[string]*par.ShardStatus) (map[string][]*par.ShardStatus, error)
 
